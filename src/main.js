9f4010e780cc26881962f5e3f6864a0ab734641e
amir <amirtaki623@gmail.com>
2025-07-14 01:26:18 +0330

    click navExpand js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 const navExpand = document.getElementById('nav-expand')
 const navExpandList = document.getElementById('nav-expand-list')
 const navExpandIcon = document.getElementById('nav-expand-icon')
+
+navExpand.addEventListener('click', (e)=> {
+    navExpandList.classList.toggle('show-list')
+    navExpandIcon.classList.toString('rotate-icon')
+})
